8f63b1da9dea220358dd439436651cdf3ec19c89
ivanvikvik <ivanvikvik@gmail.com>
2025-02-20 16:58:15 +0300

    Add project files.

---
+++ Task01Project/test_runner.cpp
@@ -0,0 +1,23 @@
+
+//Lesson42Solution
+//	- Task01
+//	- Task02
+//	- ...
+//
+//Homework42BasicSolution
+//	- Task01
+//	- Task02
+//	- ...
+//Homework42MainSolution --> Homework42MainProject
+//	- Task01
+//	- Task02
+//	- ...
+//Homework42AdvanceSolution --> Homework42AdvanceProject
+//	- Task01
+//	- Task02
+//	- ...
+#include "test.h"
+
+void main() {
+
+}
